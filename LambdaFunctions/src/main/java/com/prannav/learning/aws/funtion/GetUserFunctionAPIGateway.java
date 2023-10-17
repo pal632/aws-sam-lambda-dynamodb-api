a227d01a9f73ff39c7501293247b8c2656004583
prannavshr@gmail.com <54006033+pal632@users.noreply.github.com>
2023-10-17 00:26:51 -0500

    -adding get function

---
+++ LambdaFunctions/src/main/java/com/prannav/learning/aws/funtion/GetUserFunctionAPIGateway.java
@@ -0,0 +1,72 @@
+package com.prannav.learning.aws.funtion;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.prannav.learning.aws.funtion.model.UserData;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.utils.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetUserFunctionAPIGateway implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final Map<String, String> HEADERS = new HashMap<>();
+
+    static {
+
+        HEADERS.put("Content-Type", "application/json");
+        HEADERS.put("X-Custom-Header", "application/json");
+    }
+
+    private static final String TABLE_NAME = System.getenv("TABLE_NAME");
+    private static final Region REGION = Region.of(System.getenv("REGION_NAME"));
+    private final DynamoDbClient dynamoDbClient = DynamoDbClient.builder()
+                                                                .region(REGION)
+                                                                .build();
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        final LambdaLogger logger = context.getLogger();
+        logger.log("Received request for " + input.getResource());
+        final Map<String, String> queryParams = input.getQueryStringParameters();
+        final String userId = queryParams == null ? null: StringUtils.trim(queryParams.get("userId"));
+
+
+        if (userId == null || userId.equals("")) {
+            return new APIGatewayProxyResponseEvent().withHeaders(HEADERS)
+                                                     .withStatusCode(400)
+                                                     .withBody("'userId' missing");
+        }
+        try {
+            final DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder().
+                                                                                dynamoDbClient(dynamoDbClient)
+                                                                                .build();
+            final DynamoDbTable<UserData> userTable = enhancedClient.table(TABLE_NAME, TableSchema.fromBean(UserData.class));
+            final UserData userData = userTable.getItem(Key.builder()
+                                                           .partitionValue(userId)
+                                                           .build());
+            if (userData == null) {
+                return new APIGatewayProxyResponseEvent().withHeaders(HEADERS)
+                                                         .withStatusCode(404)
+                                                         .withBody("Not Found");
+            }
+
+            return new APIGatewayProxyResponseEvent().withHeaders(HEADERS)
+                                                     .withStatusCode(200)
+                                                     .withBody(OBJECT_MAPPER.writeValueAsString(userData));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
